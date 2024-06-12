ea2a7f7b4b5ff25b8deaa4b757e5c5186f2f026c
DLong <daveallenlong@gmail.com>
2024-06-12 11:39:01 +0900

    added - dark mode for about page

--- js/darkMode.js
+++ js/darkMode.js
@@ -4,6 +4,9 @@ const body = document.getElementsByTagName("body")[0];
 const links = document.getElementsByTagName("a");
 const nav = document.querySelector(".nav");
 const cards = document.querySelectorAll(".card");
+// about page
+const imageCard = document.querySelectorAll(".image-card");
+const first = document.querySelector(".first");
 
 function lightMode() {
 	if (sunIcon.classList.contains("fa-sun")) {
@@ -13,6 +16,13 @@ function lightMode() {
 		body.style.backgroundColor = "#f5f5f5";
 		container.style.color = "#000";
 		nav.style.color = "#000";
+		imageCard.forEach((image) => {
+			image.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
+			image.style.height = "200px";
+			image.style.borderRadius = "10px";
+			image.style.padding = " 20px";
+		});
+		first.style.border = "none";
 		cards.forEach((card) => {
 			card.style.boxShadow = "rgba(0, 0, 0, 0.35) 0px 5px 15px";
 
@@ -28,6 +38,13 @@ function lightMode() {
 		body.style.backgroundColor = "#100c08";
 		container.style.color = "#fff";
 		nav.style.color = "#fff";
+		first.style.border = "";
+		imageCard.forEach((image) => {
+			image.style.boxShadow = "";
+			image.style.height = "";
+			image.style.borderRadius = "";
+			image.style.padding = "";
+		});
 		cards.forEach((card) => {
 			card.removeEventListener("mouseenter", handleCardHover);
 			card.removeEventListener("mouseleave", handleCardUnhover);
